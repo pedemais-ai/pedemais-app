2bee0d099e936a75825f7ddff15dcc1f8f74d6b3
Alfredo Costa <alfredocosta@live.com>
2023-12-05 13:53:55 -0300

    Create user on sign-up

--- src/app/api/auth/[...nextauth]/authOptions.ts
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -1,7 +1,8 @@
-import type {NextAuthOptions} from 'next-auth'
+import type {NextAuthOptions, User} from 'next-auth'
 import GitHubProvider from 'next-auth/providers/github'
 import GoogleProvider from 'next-auth/providers/google'
 import {GITHUB_ID, GITHUB_SECRET, GOOGLE_ID, GOOGLE_SECRET} from "@/constants";
+import {prisma} from "@/prisma";
 
 export const authOptions: NextAuthOptions = {
     providers: [
@@ -14,5 +15,25 @@ export const authOptions: NextAuthOptions = {
             clientSecret: GOOGLE_SECRET,
         }),
     ],
+    callbacks: {
+        async signIn({user}: { user: User }) {
+            const existingUser = await prisma.user.findUnique({
+                where: {
+                    email: String(user.email)
+                },
+            });
+
+            if (!existingUser) {
+                await prisma.user.create({
+                    data: {
+                        name: String(user.name),
+                        email: String(user.email),
+                    },
+                });
+            }
+
+            return '/dashboard';
+        },
+    },
     debug: false
 }
