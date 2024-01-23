4c21b06da8c2a758194d66cd2f53f51ca72ddd89
Alfredo Costa <alfredocosta@live.com>
2024-01-23 19:55:58 -0300

    feat: AppIcon component

---
+++ src/components/app/AppIcon.tsx
@@ -0,0 +1,10 @@
+import {FontAwesomeIcon, FontAwesomeIconProps} from "@fortawesome/react-fontawesome";
+import React from "react";
+
+export default function AppIcon(props: FontAwesomeIconProps) {
+    return (
+        <>
+            <FontAwesomeIcon {...props}/>
+        </>
+    );
+};
