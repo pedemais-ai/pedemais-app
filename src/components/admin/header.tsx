9bb7a385653bdd7a409e25418a316cfdce1a0ad3
roberto <roberto@snow.com>
2024-01-24 00:12:17 -0300

    fix: navbar

--- src/components/admin/header.tsx
+++ src/components/admin/header.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import React, {useState} from "react";
-import {Col, Container, Nav, NavDropdown, Navbar, Row} from "react-bootstrap";
+import React, { useState } from "react";
+import { Col, Container, Nav, NavDropdown, Navbar, Row } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUtensils, faClipboardList } from "@fortawesome/free-solid-svg-icons";
+import { faUtensils, faClipboardList, faUser } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
 
 export default function Header() {
@@ -12,9 +12,9 @@ export default function Header() {
     const getCurrentDateTime = () => {
         const currentTime = new Date();
 
-        const dayOfWeek = new Intl.DateTimeFormat('pt-BR', {weekday: 'long'}).format(currentTime);
-        const dayOfMonth = new Intl.DateTimeFormat('pt-BR', {day: 'numeric'}).format(currentTime);
-        const month = new Intl.DateTimeFormat('pt-BR', {month: 'short'}).format(currentTime);
+        const dayOfWeek = new Intl.DateTimeFormat('pt-BR', { weekday: 'long' }).format(currentTime);
+        const dayOfMonth = new Intl.DateTimeFormat('pt-BR', { day: 'numeric' }).format(currentTime);
+        const month = new Intl.DateTimeFormat('pt-BR', { month: 'short' }).format(currentTime);
 
         const formattedTime = currentTime.toLocaleTimeString('pt-BR', {
             hour: 'numeric',
@@ -29,7 +29,7 @@ export default function Header() {
         };
     };
 
-    const {dayOfWeek, dayOfMonth, month, formattedTime} = getCurrentDateTime();
+    const { dayOfWeek, dayOfMonth, month, formattedTime } = getCurrentDateTime();
 
     return (
         <>
@@ -41,38 +41,54 @@ export default function Header() {
                         </Navbar.Brand>
                     </Link>
 
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" onClick={() => setExpanded(!expanded)}/>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" onClick={() => setExpanded(!expanded)} />
 
                     <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="mr-auto">
-                    <Nav.Link as={Link} href={"/admin/delivery"}>
-                        <FontAwesomeIcon icon={faUtensils} className="me-2" />
-                        Restaurante
-                    </Nav.Link>
-                    <Nav.Link as={Link} href={"/admin/products"}>
-                        <FontAwesomeIcon icon={faClipboardList} className="me-2" />
-                        Gestor de Cardápio
-                    </Nav.Link>
-                    </Nav>
-                    <div className="position-absolute end-0 text-white d-flex align-items-center me-2">
-                        <NavDropdown title="Admin" id="basic-nav-dropdown" className="me-2">
-                            <NavDropdown.Item href="#minha-conta">Minha Conta</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#sair">Sair</NavDropdown.Item>
-                        </NavDropdown>
-                        <div className="text-right">
-                            <Row className="mb-0">
-                                <Col className="text-end mb-0" style={{ lineHeight: '1', fontSize: '0.75rem' }}>
-                                    <small>{dayOfWeek.toUpperCase()}</small>
-                                    <br />
-                                    <small>{dayOfMonth} {month.toUpperCase()}</small>
-                                </Col>
-                            </Row>
+                        <Nav className="mr-auto">
+                            <Nav.Link as={Link} href={"/admin/delivery"}>
+                                <FontAwesomeIcon icon={faUtensils} className="me-2" />
+                                Restaurante
+                            </Nav.Link>
+                            <Nav.Link as={Link} href={"/admin/products"}>
+                                <FontAwesomeIcon icon={faClipboardList} className="me-2" />
+                                Gestor de Cardápio
+                            </Nav.Link>
+                        </Nav>
+                        <div className="position-absolute end-0 text-white d-flex align-items-center me-2">
+
+                            <div className="text-right">
+                                <Row className="mb-0">
+                                    <Col className="text-end mb-0" style={{ lineHeight: '1', fontSize: '0.75rem' }}>
+                                        <small>{dayOfWeek.toUpperCase()}</small>
+                                        <br />
+                                        <small>{dayOfMonth} {month.toUpperCase()}</small>
+                                    </Col>
+                                </Row>
+                            </div>
+                            <div className="text-right ms-1 me-4">
+                                <p className="mb-0 fs-3">{formattedTime}</p>
+                            </div>
+                            <div className="position-static me-2">
+                                {/* Menu Dropdown "Admin" */}
+                                <NavDropdown
+                                    title={
+                                        <>
+                                            <FontAwesomeIcon icon={faUser} style={{ marginRight: '8px' }} />
+                                            Admin
+                                        </>
+                                    }
+                                    id="admin-dropdown"
+                                    // @ts-ignore
+                                    drop="down"
+                                    align="end"
+                                    style={{ marginLeft: 'auto' } as React.CSSProperties}
+                                >
+                                    <NavDropdown.Item href="#minha-conta">Minha Conta</NavDropdown.Item>
+                                    <NavDropdown.Divider />
+                                    <NavDropdown.Item href="#sair">Sair</NavDropdown.Item>
+                                </NavDropdown>
+                            </div>
                         </div>
-                        <div className="text-right">
-                            <p className="mb-0 fs-3 ms-1">{formattedTime}</p>
-                        </div>
-                    </div>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
