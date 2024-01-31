161335badcb99e20c580cad5296a51ab9adae496
Alfredo Costa <alfredocosta@live.com>
2024-01-31 18:47:59 -0300

    fix: show cart data in menu bottom

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,35 +1,43 @@
 "use client";
 
-import { Store } from ".prisma/client";
-import React, { Suspense, useEffect, useState } from "react";
+import {Store} from ".prisma/client";
+import React, {Suspense, useEffect, useState} from "react";
 import MenuHeader from "@/components/menu/Header";
-import { Container } from "react-bootstrap";
-import { useStore } from "@/core/hooks/useStore";
-import { Prisma } from "@/core/types/prisma";
+import {Container} from "react-bootstrap";
+import {useStore} from "@/core/hooks/useStore";
+import {Prisma} from "@/core/types/prisma";
 import MenuItemList from "@/components/menu/ItemList";
 import Loading from "@/components/Loading";
-
-import { faClipboardList, faShoppingBag, faUser, faUtensils } from '@fortawesome/free-solid-svg-icons';
+import {faClipboardList, faShoppingBag, faUser, faUtensils} from '@fortawesome/free-solid-svg-icons';
 import Link from "next/link";
 import AppIcon from "@/components/app/AppIcon";
+import {useCart} from "@/core/hooks/useCart";
+import {formatCurrency} from "@/core/functions";
 
 const BottomMenu = () => {
-    return (
+    const cartState = useCart();
 
+    const [cart, setCart] = useState<Prisma.Cart | null>();
+
+    useEffect(() => {
+        cartState.get().then((p: Prisma.Cart | null | undefined) => setCart(p))
+    }, [cartState]);
+
+    return (
         <nav className="fixed-bottom bg-light">
             <div className="w-100 bg-primary py-0">
-                <Link href="/cart" className="link-offset-2 link-underline link-underline-opacity-0">
+                <Link href={"/cart"} className="link-offset-2 link-underline link-underline-opacity-0">
                     <div className="container p-2 text-white d-flex align-items-center">
                         <div className="position-relative text-center flex-grow-1">
-                            <AppIcon icon={faShoppingBag} size="lg" className="mr-2" />
+                            <AppIcon icon={faShoppingBag} size="lg" className="mr-2"/>
                             <span className="ms-1 position-absolute top-0 translate-middle badge rounded-pill bg-danger">
-                                <small>5</small>
+                                <small>{cart?.items?.length}</small>
                             </span>
                         </div>
                         <div className="flex-grow-1 text-left">
                             <small>
                                 <div className="fw-light mb-0">Total sem entrega</div>
-                                <div className="fw-bold">R$38,90</div>
+                                <div className="fw-bold">{formatCurrency(cart?.totalPrice || 0)}</div>
                             </small>
                         </div>
                         <div className="text-center">
@@ -42,20 +50,20 @@ const BottomMenu = () => {
             <div className="container d-flex justify-content-center py-2">
                 <div className="flex-fill d-flex flex-column align-items-center">
                     <div className="text-center">
-                        <AppIcon icon={faUtensils} size="lg" />
-                        <span style={{ lineHeight: 1, fontSize: 9 }} className="link-secondary d-block mt-1">Cardápio</span>
+                        <AppIcon icon={faUtensils} size="lg"/>
+                        <span style={{lineHeight: 1, fontSize: 9}} className="link-secondary d-block mt-1">Cardápio</span>
                     </div>
                 </div>
                 <div className="flex-fill d-flex flex-column align-items-center mx-5">
                     <div className="link-secondary text-center">
-                        <AppIcon icon={faClipboardList} size="lg" />
-                        <span style={{ lineHeight: 1, fontSize: 9 }} className="link-secondary d-block mt-1">Pedidos</span>
+                        <AppIcon icon={faClipboardList} size="lg"/>
+                        <span style={{lineHeight: 1, fontSize: 9}} className="link-secondary d-block mt-1">Pedidos</span>
                     </div>
                 </div>
                 <div className="flex-fill d-flex flex-column align-items-center">
                     <div className="link-secondary text-center">
-                        <AppIcon icon={faUser} size="lg" />
-                        <span style={{ lineHeight: 1, fontSize: 9 }} className="link-secondary d-block mt-1">Perfil</span>
+                        <AppIcon icon={faUser} size="lg"/>
+                        <span style={{lineHeight: 1, fontSize: 9}} className="link-secondary d-block mt-1">Perfil</span>
                     </div>
                 </div>
             </div>
@@ -64,7 +72,7 @@ const BottomMenu = () => {
     );
 };
 
-export default function Menu({ id }: { id: number }) {
+export default function Menu({id}: { id: number }) {
 
     const storeState = useStore();
     const [store, setStore] = useState<Store | null>()
@@ -74,12 +82,13 @@ export default function Menu({ id }: { id: number }) {
     }, [id, storeState]);
 
     return (<>
-        <Suspense fallback={<Loading />}>
+        <Suspense fallback={<Loading/>}>
             <Container>
-                <MenuHeader store={store as Prisma.Store} />
-                <MenuItemList store={store as Prisma.Store} />
-                <BottomMenu />
-                <div className="m-5 h-100">&nbsp;</div> {/* spacer for bottom menu */}
+                <MenuHeader store={store as Prisma.Store}/>
+                <MenuItemList store={store as Prisma.Store}/>
+                <BottomMenu/>
+                <div className="m-5 h-100">&nbsp;</div>
+                {/* spacer for bottom menu */}
             </Container>
         </Suspense>
     </>)
