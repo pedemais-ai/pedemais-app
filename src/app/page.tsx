3457992936ff613329fa792f57dad3b39489876f
Alfredo Costa <alfredocosta@live.com>
2024-05-26 15:58:02 -0300

    feat: Request login on access homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,13 @@
 import {authOptions} from "./api/auth/[...nextauth]/authOptions"
 import {getServerSession} from "next-auth/next"
-import Link from "next/link";
+import {redirect} from "next/navigation";
 
 export default async function Home() {
     const session = await getServerSession(authOptions)
 
-    return (
-        <>
-            {session ? (
-                <>
-                    Hello {session?.user?.name}!
-                </>
-            ) : (
-                <>
-                    <Link href={"/api/auth/signin"}>Sign-in</Link>
-                    <Link href={"/api/auth/register"}>Sign-up</Link>
-                </>
-            )}
-        </>
-    )
+    if (!session) {
+        redirect('/api/auth/signin?callbackUrl=/admin')
+    }
+
+    redirect('/admin')
 }
