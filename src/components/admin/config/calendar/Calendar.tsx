e93ffcc08ba6bedd6a706ea1178b7d392717a574
Alfredo Costa <alfredocosta@live.com>
2024-05-28 15:55:41 -0300

    feat: Add mock feriados to calendar component

--- src/components/admin/config/calendar/Calendar.tsx
+++ src/components/admin/config/calendar/Calendar.tsx
@@ -1,20 +1,20 @@
 "use client";
 
 import React, {useEffect, useState} from "react";
-import {Card, Container} from "react-bootstrap";
+import {Button, Card, Col, Container, Row} from "react-bootstrap";
 import {Prisma} from "@/core/types/prisma";
 import {useMe} from "@/core/hooks/useMe";
-import styles from './calendar.module.css';
+import styles from "./calendar.module.css";
+import Link from "next/link";
 
 export default function Calendar() {
-
     const [me, setMe] = useState<Prisma.User>();
     const meState = useMe();
 
     const DAYS = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
     const DAYS_LEAP = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    const DAYS_OF_THE_WEEK = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'];
-    const MONTHS = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
+    const DAYS_OF_THE_WEEK = ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado', 'Domingo'];
+    const MONTHS = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
 
     const today = new Date();
     const [date, setDate] = useState<Date>(today);
@@ -41,41 +41,56 @@ export default function Calendar() {
 
     const days = isLeapYear(year) ? DAYS_LEAP : DAYS;
 
-
     return (
         <>
             <Container>
-                <Card className="mt-3" style={{marginBottom: "100px"}}>
-                    <div className={styles.frame}>
-                        <div className={styles.header}>
-                            <div className={styles.button} onClick={() => setDate(new Date(year, month - 1, day))}>Prev</div>
-                            <div>
-                                {MONTHS[month]} {year}
+                <Card className="mt-3 mb-5">
+                    <Card.Body>
+                        <Row className="d-flex justify-content-between align-items-center mb-3">
+                            <Col className="text-center">
+                                <Button variant="secondary" onClick={() => setDate(new Date(year, month - 1, day))}>
+                                    Anterior
+                                </Button>
+                            </Col>
+                            <Col className="text-center">
+                                <h5>{MONTHS[month]} de {year}</h5>
+                            </Col>
+                            <Col className="text-center">
+                                <Button variant="secondary" onClick={() => setDate(new Date(year, month + 1, day))}>
+                                    Próximo
+                                </Button>
+                            </Col>
+                        </Row>
+                        <Row className="text-center">
+                            <div className={styles.body}>
+                                {DAYS_OF_THE_WEEK.map((d) => (
+                                    <div key={d} className={styles.weekDay}>
+                                        <strong>{d}</strong>
+                                    </div>
+                                ))}
+                                {Array(days[month] + (startDay - 1))
+                                    .fill(null)
+                                    .map((_, index) => {
+                                        const d = index - (startDay - 2);
+                                        return (
+                                            <div
+                                                key={index}
+                                                className={`${styles.day} ${(d === today.getDate() && month === today.getMonth()) ? styles.isToday : ''} ${d === day ? styles.isSelected : ''}`}
+                                                // onClick={() => setDate(new Date(year, month, d))}
+                                            >
+                                                {d > 0 ? <>
+                                                    <span className={"w-25"}>{d}</span>
+                                                    <div className={"d-block"}>
+                                                        {d === 1 ? <Link href={"#"} className={"text-secondary d-block mb-1"}>Feriadim</Link> : ''}
+                                                        {d === 23 ? <Link href={"#"} className={"text-secondary d-block mb-1"}>Feriadim</Link> : ''}
+                                                    </div>
+                                                </> : ''}
+                                            </div>
+                                        );
+                                    })}
                             </div>
-                            <div className={styles.button} onClick={() => setDate(new Date(year, month + 1, day))}>Next</div>
-                        </div>
-                        <div className={styles.body}>
-                            {DAYS_OF_THE_WEEK.map((d) => (
-                                <div key={d} className={styles.day}>
-                                    <strong>{d}</strong>
-                                </div>
-                            ))}
-                            {Array(days[month] + (startDay - 1))
-                                .fill(null)
-                                .map((_, index) => {
-                                    const d = index - (startDay - 2);
-                                    return (
-                                        <div
-                                            key={index}
-                                            className={`${styles.day} ${d === today.getDate() ? styles.isToday : ''} ${d === day ? styles.isSelected : ''}`}
-                                            onClick={() => setDate(new Date(year, month, d))}
-                                        >
-                                            {d > 0 ? d : ''}
-                                        </div>
-                                    );
-                                })}
-                        </div>
-                    </div>
+                        </Row>
+                    </Card.Body>
                 </Card>
             </Container>
         </>
